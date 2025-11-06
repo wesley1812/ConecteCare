71b66a077e4b4a22e362c198d87c6ba33a91a667
wda1812 <wda1812@hotmail.com>
2025-11-06 11:15:06 -0300

    teste nOK-2

--- ConecteCare/vite.config.ts
+++ ConecteCare/vite.config.ts
@@ -5,4 +5,13 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    headers: {
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin',
+    }
+  },
+  build: {
+    target: 'esnext' // Importante para WebAssembly
+  }
 });
